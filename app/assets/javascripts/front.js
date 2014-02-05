f4dc0c33d4975f874b79980afd5590ffef43a839
Kristen Boydstun <kristen.boydstun@gmail.com>
2014-02-04 18:10:32 -0800

    add item drag and drop

--- app/assets/javascripts/front.js
+++ app/assets/javascripts/front.js
@@ -1,3 +1,20 @@
-$(function() {
-$('#myModal').modal()
+$(document).ready(function(){
+
+  $(".draggable").draggable({stack: ".draggable"});
+  $( ".droppable" ).droppable({
+    drop: function( event, ui ) {
+      $( this )
+        .addClass( "ui-state-highlight" )
+        .find( "p" )
+          .html( "Dropped!" );
+    },
+    over: function(event, elem) {
+              $(this).addClass("over");
+              console.log("over");
+           },
+    out: function(event, elem) {
+              $(this).removeClass("over");
+           }
+  });
+
 });
